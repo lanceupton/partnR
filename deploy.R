faa761f854289a110e0bb285b969b45553b5d8e9
Lance Upton <lance.upton@gmail.com>
2021-04-15 19:47:15 -0400

    Update packages and ignore files.

--- deploy.R
+++ deploy.R
@@ -17,6 +17,6 @@ setAccountInfo(name   = error_on_missing_name("SHINY_ACC_NAME"),
 
 # Deploy the application.
 deployApp(
-    appFiles = setdiff(list.files(), "renv"),
+    appFiles = NULL,
     appName = error_on_missing_name("MASTER_NAME")
 )
